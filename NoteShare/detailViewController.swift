225d78dd8fb64c2df4e8e9a23714db4fc459d1dc
daghrty2 <daghrty2@illinois.edu>
2016-02-22 15:58:56 -0600

    Added list, detail view

---
+++ NoteShare/detailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  noteShareVC.swift
+//  NoteShare
+//
+//  Created by Melanie Daugherty on 2/20/16.
+//  Copyright © 2016 #teambae. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class detailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- NoteShare/noteShareVC.swift
+++ NoteShare/noteShareVC.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  ViewController.swift
 //  NoteShare
@@ -8,24 +10,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var POOFIELD: UITextField!
-    @IBOutlet weak var SendButton: UIButton!
-    @IBOutlet weak var HeyLabel: UILabel!
+var objects: [String] = [String]()
+var currentIndex: Int = 0
+var masterView: noteShareVC?
+var detailView: detailViewController?
 
+//identifier for persistant storage:
+let kNotes: String = "notes"
+
+//default text for new notes:
+let blank_note: String = "(New Note)"
+
+class noteShareVC: UITableViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var storyboardTable: UITableView!
+    
     @IBAction func sendButtonWasHit(sender: AnyObject) {
-        HeyLabel.text = "Hey, " + POOFIELD.text!
+        //        HeyLabel.text = "Hey, " + POOFIELD.text!
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
